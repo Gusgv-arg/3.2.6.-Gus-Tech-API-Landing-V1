b1a566386a86e7888a436a03830ff5359b4abf73
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2024-04-27 09:36:19 -0300

    server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import express from "express"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
 import megaBotRouter from "./routes/megaBotRouter.js";
@@ -9,13 +9,10 @@ dotenv.config();
 
 //Connect with DB
 try {
-  mongoose.connect(process.env.MONGODB_URI).then(()=>{
-    console.log("Connected to MegaBot data base!")
-    }).catch(err=>{
-      console.log(err.message)
-    })  
+	await mongoose.connect(process.env.MONGODB_URI);
+	console.log("Connected to MegaBot data base!");
 } catch (error) {
-  console.log("Error while connecting with DB", error.message)
+	console.log("Error while connecting with DB", error.message);
 }
 
 // Server creation
@@ -23,12 +20,18 @@ const app = express();
 
 // Cors config
 app.use(
-  cors({
-    origin: ["http://localhost:3000", "https://api-landingpage.onrender.com"],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
-  })
+	cors({
+		origin: ["http://localhost:3000", "https://api-landingpage.onrender.com"],
+		credentials: true,
+		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+		allowedHeaders: [
+			"Origin",
+			"X-Requested-With",
+			"Content-Type",
+			"Accept",
+			"Authorization",
+		],
+	})
 );
 
 // Middlewares
@@ -37,12 +40,11 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use("/megabot", megaBotRouter);  
- 
+app.use("/megabot", megaBotRouter);
+
 // Port
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
 	console.log(`Server running at http://localhost:${port}`);
 });
-
