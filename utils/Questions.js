0784340b283212b065b03c0579adb45464e156c6
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2024-05-07 10:11:01 -0300

    preguntas

--- utils/Questions.js
+++ utils/Questions.js
@@ -1,11 +1,9 @@
-export const question1 =
-	"1. ¿Cómo se hace para que el Chatbot sepa de mi negocio?";
-export const answerQuestion1 =
-	"1. Se realiza lo que se denomina entrenamiento; que básicamente consiste en proveer al modelo con la información de su empresa y al caso de uso. Esta información puede provenir de la página web, archivos (word, pdf, txt, etc).";
+export const answerQuestion1 = "1. Imagine tener un Asistente que sepa todo sobre su negocio, disponible 24hs los 365 días del año dispuesto a atender a sus clientes o empleados. Las tareas pueden ir desde responder consultas, tomar pedidos, agendar reuniones, capacitación, etc. Haga el cálculo de los tiempos que podría ahorrar en su empresa delegando tareas repetitivas liberando el tiempo de sus empleados para que vendan mas. Piense en sus clientes recibiendo respuestas de manera inmediata. O un empleado nuevo al que hay que capacitar con información que se fue con la persona que ocupaba el puesto. Estos son algunos ejemplos; nostros estamos para ayudarlo en su caso de uso.";
 
-export const question2 = "2. ¿Cuánto se tarda en implementar un Chatbot de IA?";
-export const answerQuestion2 =
-	"2. La implementación lleva unas horas si la base de información para el entrenamiento se encuentra disponible y si se trata solo de incorporar el Chatbot a una web. Luego viene una etapa de monitoreo y de ajustes que puede demorar entre una a tres semanas.";
+export const answerQuestion2 = "2. Se realiza lo que se denomina entrenamiento; que básicamente consiste en proveer al modelo de Inteligencia Artificial con la información de su empresa y darle las instrucciones para que realice las tares que usted pretende. La información puede provenir de la página web, archivos Word, pdf, txt, videos, etc.";
 
-export const question3 = "3. ¿Cuánto sale implementar un Chatbot de IA?";
-export const answerQuestion3 = "3. La integración de un Chatbot en una página web sale u$ 200 por única vez. Luego hay un costo fijo mensual de u$ 20 más un costo por mensaje que oscila entre u$ 0,01 y u$ 0,03 que se paga al proveedor del modelo de Inteligencia Artificial.";
+export const answerQuestion3 = "3. Lo invito a hacer un análisis de sus procesos. Pregunte a sus empleados cuanto tiempo invierten realizando tareas repetitivas. Si por ello a veces se equivocan y vuelven a hacer el trabajo. Si tienen que acceder a distintos sistemas para realizar algún informe. Para resolver estas cuestiones, podemos ayudarlo en la detección de oportunidades y automatizar procesos con la ayuda de la tecnología."
+
+export const answerQuestion4 = "4. La implementación lleva unas horas si la base de información para el entrenamiento se encuentra disponible y si se trata solo de incorporar el Chatbot a una web. Luego viene una etapa de monitoreo y de ajustes que puede demorar entre una a tres semanas.";
+
+export const answerQuestion5 = "5. La integración de un Chatbot en una página web sale u$ 200 por única vez. Luego hay un costo fijo mensual de u$ 20 más un costo por mensaje que oscila entre u$ 0,01 y u$ 0,03 que se paga al proveedor del modelo de Inteligencia Artificial.";
