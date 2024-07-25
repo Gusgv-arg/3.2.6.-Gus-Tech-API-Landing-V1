9f3b1b5424331a7a663b50aee45489374e45973f
Gustavo Gómez Villafañe <gusgvillafane@gmail.com>
2024-07-25 19:20:09 -0300

    texttoaudio

--- utils/textToAudio.js
+++ utils/textToAudio.js
@@ -2,6 +2,7 @@ import fs from "fs";
 import path from "path";
 import OpenAI from "openai";
 import dotenv from "dotenv";
+import { fileURLToPath } from 'url';
 dotenv.config();
 
 const API_KEY = process.env.API_KEY_CHATGPT;
@@ -12,20 +13,27 @@ const openai = new OpenAI({
 	project: "proj_cLySVdd60XL8zbjd9zc8gGMH",
 });
 
-const speechFile = path.resolve(__dirname, '..', 'assets', 'speech.mp3');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const assetsDir = path.resolve(__dirname, '..', 'assets');
+if (!fs.existsSync(assetsDir)){
+    fs.mkdirSync(assetsDir);
+}
+const speechFile = path.join(assetsDir, 'speech.mp3');
 
 async function textToAudio(content) {
-	console.log("Comienzo a ejecutar textToAudio!!");
-	const mp3 = await openai.audio.speech.create({
-		model: "tts-1",
-		voice: "onyx",
-		input: content,
-	});
-	console.log(speechFile);
-	const buffer = Buffer.from(await mp3.arrayBuffer());
-	await fs.promises.writeFile(speechFile, buffer);
+    console.log("Comienzo a ejecutar textToAudio!!");
+    const mp3 = await openai.audio.speech.create({
+        model: "tts-1",
+        voice: "onyx",
+        input: content,
+    });
+    console.log(speechFile);
+    const buffer = Buffer.from(await mp3.arrayBuffer());
+    await fs.promises.writeFile(speechFile, buffer);
 
-	return speechFile;
+    return speechFile;
 }
 
 export default textToAudio;
